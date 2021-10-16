bc45692955aaa13032511352b39b9f11cf90c0bc
Vishal-sarkar <74032678+Vishal-sarkar@users.noreply.github.com>
2021-10-16 21:37:50 +0530

    deploying my facerecognition app

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				{'Enter URL of your image'}
+			</div>
+			<div className='white f1'>
+				{'I will detect your face.'}
+			</div>
+		</div>
+	);
+}
+export default  Rank;
